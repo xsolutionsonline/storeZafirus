0f45f5046e294919f25d839e2f92133d91808ce7
xsolutionsonline <xsolutionsonline@gmail.com>
2024-04-18 11:13:16 -0500

    creacion del crud de productos , conexion a base de datos mysql  e implementacion de typeorm

--- src/presentation/api/app.module.ts
+++ src/presentation/api/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "../../app.controller";
-import { AppService } from "../../app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductsService } from "src/application/services/products/products.service";
+import { ProductsController } from "src/infrastructure/controllers/products/products.controller";
+import { ProductEntity } from "src/infrastructure/persistence/entities/products/product.entity";
+import { ProductRepositoryImpl } from "../../infrastructure/persistence/repositories/products/product.repository";
+import { config } from "dotenv";
+config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity]),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST || "",
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USERNAME || "",
+      password: process.env.DB_PASSWORD || "",
+      database: process.env.DB_DATABASE || "",
+      autoLoadEntities: true,
+      entities: [ProductEntity],
+      synchronize: true,
+    }),
+  ],
+  controllers: [ProductsController],
+  providers: [ProductsService, ProductRepositoryImpl],
 })
 export class AppModule {}
