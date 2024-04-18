dca9d7192df3b7a9353501683dc31bd345b0deba
xsolutionsonline <xsolutionsonline@gmail.com>
2024-04-18 04:51:46 -0500

    estrutuca de carpetas con el fin de iplementar arquitectura hexagonal

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/presentation/api/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
